faaff314bf0523ada7ade34ec3f6a8ac9d0dd3dd
belaneb <belane.belayhun@gmail.com>
2023-04-12 22:08:07 -0500

    contant added

--- script.js
+++ script.js
@@ -1,53 +1,26 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
-
-
 //current day is displayed at the top of the calendar
 var todaysDate = dayjs().format('MMM D, YYYY');
 $("#currentDay").text(todaysDate);
 console.log(todaysDate);
 
 //timeblocks for standard business hours
-
 var now = new Date();
 var currentHour = now.getHours();
-//var currentHour = 13
-
+//TEST: var currentHour = 13
 console.log(currentHour);
 
 //each timeblock is color coded to indicate whether it is in the past, present, or future
-
 /*add & remove class here*/
 
-// step 1: grab HTML elements
+//Grab HTML elements
 var timeBlocks = $(".time-block")
 console.log(timeBlocks)
 
-
+//.each: aka for loop
+//split "hour-9", to "hour" & "9", [1] grabs 9.
 timeBlocks.each(function(timeblock) {
   var hour = $(this).attr("id").split("-")[1]
-  //console.log(parseInt(currentHour)>parseInt(hour))
+
   if(currentHour > hour){
     $(this).addClass("past")
     $(this).removeClass("present", "future")
@@ -59,20 +32,15 @@ timeBlocks.each(function(timeblock) {
   if(currentHour == hour){
     $(this).addClass("present")
     $(this).removeClass("past", "future")
-
   }
 });
+
 var userInput = $("<textarea>")
 console.log(userInput)
 
-localStorage.setItem(userInput, "userInput")
-console.log(userInput)
-
-localStorage.getItem(userInput, "userInput")
-console.log(localStorage)
+localStorage.setItem("userInput", JSON.stringify(userInput))
+var savedUserInput = JSON.parse(localStorage.getItem("userInput"))
 
 var saveButton = $(".saveBtn")
 saveButton.click(function(){
-  console.log(saveButton)
 })
-
